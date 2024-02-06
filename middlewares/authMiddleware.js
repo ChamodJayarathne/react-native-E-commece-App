7edac752ce1b4d96ae09be3b591eabdf1cef0888
ChamodJayarathne <148670748+ChamodJayarathne@users.noreply.github.com>
2024-02-07 00:52:01 +0530

    production code
    nodejs e-commerce app

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import JWT from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+export const isAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+  //validation
+  if (!token) {
+    return res.status(401).send({
+      success: false,
+      message: "Unauthorized User",
+    });
+  }
+  const decodeData = JWT.verify(token, process.env.JWT_SECRET);
+  req.user = await userModel.findById(decodeData._id);
+  next();
+};
+
+//Admin Auth
+export const isAdmin = async (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(401).send({
+      success: false,
+      message: "admin only",
+    });
+  }
+  next();
+};
